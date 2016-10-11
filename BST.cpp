0f220d877e894e24dc76643808d3a7aacd06cea2
markymark5127 <markmayne@ymail.com>
2016-10-11 14:03:57 -0400

    I made all the functions

--- BST.cpp
+++ BST.cpp
@@ -8,63 +8,100 @@
 /* **************************************************************** */
 
 #if ALL || CONSTRUCTOR
-//template <class T>
-//BST<T>::BST()
-//{
+template <class T>
+BST<T>::BST()
+{
     // TODO: Initialize root
-//}
+    root = new Node<T>();
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || DESTRUCTOR
-//template <class T>
-//BST<T>::~BST()
-//{
+template <class T>
+BST<T>::~BST()
+{
     // TODO: Remove any nodes
-//}
+    delete root;
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || ISEMPTY
 // TODO: isEmpty() method
+template <class T>
+bool BST::isEmpty()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FIND
 // TODO: find() method
+template <class T>
+bool BST::find(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDMIN
 // TODO: findMin() method
+template <class T>
+const T & BST::findMin()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDMAX
 // TODO: findMax() method
+template <class T>
+const T & BST::findMax()
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || INSERT
 // TODO: insert() method
+template <class T>
+void BST::insert(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || REMOVE
 // TODO: remove() method
+template <class T>
+void BST::remove(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || MAKEEMPTY
 // TODO: makeEmpty() method
+template <class T>
+void BST::makeEmpty()
+{
+
+}
 #endif
 
 /* **************************************************************** */
@@ -76,42 +113,78 @@
 /* **************************************************************** */
 #if ALL || FINDNODE
 // TODO: findNode() private method
+template <class T>
+Node<T> * BST::findNode(Node<T> * node,const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDMINNODE
 // TODO: findMinNode() private method
+template <class T>
+Node<T> * BST::findMinNode(Node<T> * node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDMAXNODE
 // TODO: findMaxNode() private method
+template <class T>
+Node<T> * BST::findMaxNode(Node<T> * node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || INSERTNODE
 // TODO: insertNode() private method
+template <class T>
+Node<T> * BST::insertNode(Node<T> * node,const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDSUCCESSOR
 // TODO: findSuccessor() private method
+template <class T>
+Node<T> * BST::findSuccessor(Node<T> * node)
+{
+
+}
+
 #endif
 
 /* **************************************************************** */
 
 #if ALL || FINDPARENTOF
 // TODO: findParentOf() private method
+template <class T>
+Node<T> * BST::findParentOf(const T & x)
+{
+
+}
 #endif
 
 /* **************************************************************** */
 
 #if ALL || REMOVEALLNODES
 // TODO: removeAllNodes() private method
+template <class T>
+void BST::removeAllNodes(Node<T> * node)
+{
+
+}
 #endif
 
 /* **************************************************************** */
